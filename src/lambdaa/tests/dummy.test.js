fb4b0d4b71e82c7b987305e4436fdca4271759f8
Deekshith <deekshith.hegde@prettylittlething.com>
2023-10-27 18:42:07 +0530

    fix: added unit tests

---
+++ src/lambdaa/tests/dummy.test.js
@@ -0,0 +1,10 @@
+describe('Dummy Unit Tests', () => {
+    it('should pass a dummy test', () => {
+      expect(true).toBe(true);
+    });
+  
+    it('should also pass another dummy test', () => {
+      expect(false).toBe(false);
+    });
+  });
+  
